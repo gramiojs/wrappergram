be65bf8e03104881816e036436b0a4ce61faf6d6
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-31 19:48:58 +0300

    chore: bumps

---
+++ scripts/bench.ts
@@ -0,0 +1,20 @@
+import { baseline, bench, group, run } from "mitata";
+
+function callAPI(method: any, args: number) {
+	return args;
+}
+
+const cachedAPI = new Proxy({} as Record<"test", (num: number) => number>, {
+	get: (target, method) => target[method] ??= ((args) => callAPI(method, args)),
+});
+
+const api = new Proxy({} as Record<"test", (num: number) => number>, {
+	get: (target, method) => (args) => callAPI(method, args),
+});
+
+group("new Proxy", () => {
+	bench("cached", () => cachedAPI.test(2));
+	bench("non-cached", () => api.test(2));
+});
+
+await run();
